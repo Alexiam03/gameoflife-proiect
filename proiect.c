7e8ee2db8fabd6547a299a723ba26ef2572622db
maria.anton0312 <anton.alexia05@gmail.com>
2025-03-30 20:22:05 +0300

    Task 1 tema PA

---
+++ proiect.c
@@ -0,0 +1,251 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void calculVecini(char **matrice, int row, int col, int **aux)
+{
+    int nrVecini = 0, i, j;
+
+    for(j = 1; j < col - 1; j++)
+    {//facem verificarea pentru linia 0 
+
+        nrVecini = 0;
+        if(matrice[0][j-1] == 'X')
+            nrVecini++;
+        if(matrice[0][j+1] == 'X')
+            nrVecini++;
+        if(matrice[1][j-1] == 'X')
+            nrVecini++;
+        if(matrice[1][j] == 'X')
+            nrVecini++;
+        if(matrice[1][j+1] == 'X')
+            nrVecini++;
+        aux[0][j] = nrVecini;
+    }
+
+    for(j = 1; j < col - 1; j++)
+    {//facem verificarea pentru ultima linie
+
+        nrVecini = 0;
+        if(matrice[row-1][j-1] == 'X')
+            nrVecini++;
+        if(matrice[row-1][j+1] == 'X')
+            nrVecini++;
+        if(matrice[row-2][j-1] == 'X')
+            nrVecini++;
+        if(matrice[row-2][j] == 'X')
+            nrVecini++;
+        if(matrice[row-2][j+1] == 'X')
+            nrVecini++;
+        aux[row-1][j] = nrVecini;
+    }
+
+    for(i = 1; i < row - 1; i++)
+    {//facem verificarea pentru prima coloana
+        nrVecini = 0;
+        if(matrice[i][1] == 'X')
+            nrVecini++;
+        if(matrice[i-1][0] == 'X')
+            nrVecini++;
+        if(matrice[i-1][1] == 'X')
+            nrVecini++;
+        if(matrice[i+1][0] == 'X')
+            nrVecini++;
+        if(matrice[i+1][1] == 'X')
+            nrVecini++;
+        aux[i][0] = nrVecini;
+    }
+
+    for(i = 1; i < row - 1; i++)
+    {//facem verificarea pentru ultima coloana
+        nrVecini = 0;
+        if(matrice[i-1][col-1] == 'X')
+            nrVecini++;
+        if(matrice[i-1][col-2] == 'X')
+            nrVecini++;
+        if(matrice[i][col-2] == 'X')
+            nrVecini++;
+        if(matrice[i+1][col-2] == 'X')
+            nrVecini++;
+        if(matrice[i+1][col-1] == 'X')
+            nrVecini++;
+        aux[i][col-1] = nrVecini;
+    }
+
+
+    // facem verificarea pentru interiorul matricei
+    for( i = 1; i < row - 1; i++)
+        for( j = 1; j < col - 1; j++)
+            {
+                nrVecini = 0;
+
+                if(matrice[i][j-1] == 'X')
+                    nrVecini++;
+                if(matrice[i-1][j-1] == 'X')
+                    nrVecini++;
+                if(matrice[i-1][j] == 'X')
+                    nrVecini++;
+                if(matrice[i-1][j+1] == 'X')
+                    nrVecini++;
+                if(matrice[i][j+1] == 'X')
+                    nrVecini++;
+                if(matrice[i+1][j+1] == 'X')
+                    nrVecini++;
+                if(matrice[i+1][j] == 'X')
+                    nrVecini++;
+                if(matrice[i+1][j-1] == 'X')
+                    nrVecini++;
+
+                aux[i][j] = nrVecini;
+            }
+
+        nrVecini = 0;
+        //facem verificarea pentru colt stg sus
+        if(matrice[0][1] == 'X')
+            nrVecini++;
+        if(matrice[1][1] == 'X')
+            nrVecini++;
+        if(matrice[1][0] == 'X')
+            nrVecini++;
+        aux[0][0] = nrVecini;
+
+        nrVecini = 0;
+        //facem verificarea pentru colt dr sus
+        if(matrice[0][col-2] == 'X')
+            nrVecini++;
+        if(matrice[1][col-2] == 'X')
+            nrVecini++;
+        if(matrice[1][col-1] == 'X')
+            nrVecini++;
+        aux[0][col-1] = nrVecini;
+
+        nrVecini = 0;
+        //facem verificarea pentru colt stg jos
+        if(matrice[row-2][0] == 'X')
+            nrVecini++;
+        if(matrice[row-2][1] == 'X')
+            nrVecini++;
+        if(matrice[row-1][1] == 'X')
+            nrVecini++;
+        aux[row-1][0] = nrVecini;
+
+        nrVecini = 0;
+        //facem verificarea pentru colt dr jos
+        if(matrice[row-2][col-1] == 'X')
+            nrVecini++;
+        if(matrice[row-2][col-2] == 'X')
+            nrVecini++;
+        if(matrice[row-1][col-2] == 'X')
+            nrVecini++;
+        aux[row-1][col-1] = nrVecini;
+
+}
+
+void game ( char **matrice, int **aux, int row, int col)
+{
+    int i, j;
+
+    for(i = 0; i < row; i++)
+        for(j = 0; j < col; j++)
+            {//verificam daca respectiva casuta este vie/moarta si aplicam regulile jocului
+                if(matrice[i][j] == '+')
+                    if(aux[i][j] == 3)
+                        matrice[i][j] = 'X';
+                if(matrice[i][j] == 'X')
+                    if(aux[i][j] < 2)
+                        matrice[i][j] = '+';
+                    else if(aux[i][j] > 3)
+                        matrice[i][j] = '+';
+            }
+}
+
+int main (int argc, const char* argv[])
+{
+    int row, col, i, j, k, t;
+    //row = nr de linii 
+    //col = nr de coloane
+    FILE *fin;
+    FILE *fout;
+
+    if(argc < 3)
+    return 1;
+
+    fin = fopen(argv[1], "r");
+    if(fin == NULL)
+    {
+        printf("Nu s-a putut deschide fisierul");
+        return 0;
+    }
+
+    fout = fopen(argv[2], "w");
+    if(fout == NULL)
+    {
+        printf("Nu s-a putut deschide fisierul");
+        return 0;
+    }
+
+    fscanf(fin, "%d", &t);
+    fscanf(fin, "%d %d", &row, &col);
+    fscanf(fin, "%d", &k);
+    int **aux; // aux e folosita pentru a stoca numarul de vecini egali cu X ai matricei
+    char **matrice;
+    
+    //alocam dinamic sspatiul din matrice
+    matrice = (char**)malloc(row * sizeof(char *));
+    for( i = 0; i < row; i++)
+        matrice[i] = (char *)malloc( col * sizeof(char));
+
+    // Introduceti matricea 
+    for( i = 0; i < row; i++)
+        for( j = 0; j < col; j++)
+            fscanf(fin, " %c", &matrice[i][j]);
+    
+    fclose(fin);
+
+    aux = (int**)malloc(row * sizeof(int *));
+    for( i = 0; i < row; i++)
+        aux[i] = (int *)malloc(col * sizeof(int));
+ 
+    //afisam matricea initiala
+    for( i = 0; i < row; i++)
+        {
+            for( j = 0; j < col; j++)
+                fprintf(fout, "%c", matrice[i][j]);
+            fprintf(fout, "\n");
+        }
+
+    fprintf(fout, "\n");
+
+    while(k)
+        {
+            calculVecini(matrice, row, col, aux);
+            game(matrice, aux, row, col);
+
+            //afisam fiecare matrice noua generata
+            for( i = 0; i < row; i++)
+            {
+                for( j = 0; j < col; j++)
+                    fprintf(fout, "%c", matrice[i][j]);
+                fprintf(fout, "\n");
+            }
+
+            fprintf(fout, "\n");
+
+            k--;
+        }
+   
+ 
+
+    for( i = 0; i < row; i++)
+        {
+            free(matrice[i]);
+            free(aux[i]);
+        }
+
+    free(matrice);
+    free(aux);
+   // fclose(fin);
+    fclose(fout);
+
+    return 0;
+}
